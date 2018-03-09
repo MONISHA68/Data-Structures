4b371d901a9ea5f4063d1e1fdbb6ef04e92a933a
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-03-09 23:28:40 +0530

    Create create from last and display the nodes.c

---
+++ create from last and display the nodes.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+typedef struct node n;
+n *head=NULL;
+n *temp=NULL;
+void create()
+{
+    n *newnode;
+    int no;
+    newnode=(n*)malloc(sizeof(n));
+    scanf("%d",&no);
+    newnode->data=no;
+    newnode->next=NULL;
+    if(head==NULL)
+    {
+        head=temp=newnode;
+    }
+    else
+    {
+        newnode->next=head;
+        head=newnode;
+    }
+}
+void display()
+{
+    temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    if(temp==NULL)
+    {
+        printf("NULL");
+    }
+}
+int main(void)
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        create();
+    }
+    display();
+	return 0;
+}
